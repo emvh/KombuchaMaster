867af012873e46863d3ab07229ba0a786afc518d
emvh <emilyvho@gmail.com>
2021-01-05 07:12:03 -0800

    Add header title based on tab navigator

--- App.js
+++ App.js
@@ -2,31 +2,30 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
+import NavigationTabs from './src/components/NavigationBar.js';
+import CurrentBrews from './src/screens/CurrentBrews.js';
+import CompletedBrews from './src/screens/CompletedBrews.js';
+import { getHeaderTitle } from './src/utils/HeaderTitle';
+// import LoginScreen from './src/screens/LoginScreen.js';
 
 const Stack = createStackNavigator();
 
-function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>hi!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={NavigationTabs} options={ ({ route }) => ({ headerTitle: getHeaderTitle(route) }) }/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    // backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
