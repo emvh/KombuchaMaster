1591df698335329fae95bf732f801e4035b77cae
emvh <emilyvho@gmail.com>
2021-01-08 05:04:22 -0800

    Organize screens in stack navigator

--- App.js
+++ App.js
@@ -1,34 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import NavigationTabs from './src/components/NavigationBar.js';
+import HomeScreen from './src/screens/HomeScreen.js';
+import AddBrew from './src/screens/AddBrew.js';
 import CurrentBrews from './src/screens/CurrentBrews.js';
 import CompletedBrews from './src/screens/CompletedBrews.js';
 import { getHeaderTitle } from './src/utils/HeaderTitle';
-// import LoginScreen from './src/screens/LoginScreen.js';
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={NavigationTabs} options={ ({ route }) => ({ headerTitle: getHeaderTitle(route) }) }/>
+      <Stack.Navigator initialRouteName="Current">
+        {/* <Stack.Screen name="Home" component={NavigationTabs} options={ ({ route }) => ({
+          headerTitle: getHeaderTitle(route)}) } /> */}
+        <Stack.Screen name="Home" component={HomeScreen}
+          options={{
+            headerTitle: "Welcome Brew Master!"
+          }}
+        />
+        <Stack.Screen name="Current" component={CurrentBrews}
+          options={{
+            headerTitle: "Brews in Progress"
+          }}
+        />
+        <Stack.Screen name="Add" component={AddBrew}
+          options={{
+            headerTitle: "Add Brew"
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default App;
