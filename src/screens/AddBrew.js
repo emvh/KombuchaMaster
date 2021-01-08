8b387d9e40de2ed69ffa60475b27f10f637b0a83
emvh <emilyvho@gmail.com>
2021-01-08 04:05:24 -0800

    Create add brew screen with brew form component

---
+++ src/screens/AddBrew.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import BrewForm from '../components/BrewForm.js';
+
+const AddBrew = () => {
+  return (
+    <View style={styles.container}>
+      <Text>add brew here</Text>
+      <BrewForm />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default AddBrew;
