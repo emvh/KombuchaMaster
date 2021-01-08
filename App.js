52b14d83a7b40059b4a6a1aaf3c2e06455799896
emvh <emilyvho@gmail.com>
2021-01-08 08:15:33 -0800

    Set initial route to render to Navigation Tabs

--- App.js
+++ App.js
@@ -1,35 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
-import NavigationTabs from './src/components/NavigationBar.js';
-import HomeScreen from './src/screens/HomeScreen.js';
-import AddBrew from './src/screens/AddBrew.js';
+import { NavigationContainer } from '@react-navigation/native';
+import NavigationTabs from './src/components/NavigationTabs.js';
 import CurrentBrews from './src/screens/CurrentBrews.js';
+import AddBrew from './src/screens/AddBrew.js';
 import CompletedBrews from './src/screens/CompletedBrews.js';
-import { getHeaderTitle } from './src/utils/HeaderTitle';
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Current">
-        {/* <Stack.Screen name="Home" component={NavigationTabs} options={ ({ route }) => ({
-          headerTitle: getHeaderTitle(route)}) } /> */}
-        <Stack.Screen name="Home" component={HomeScreen}
-          options={{
-            headerTitle: "Welcome Brew Master!"
-          }}
+      <Stack.Navigator initialRouteName="Navigation">
+        <Stack.Screen
+          name="Navigation"
+          component={NavigationTabs}
         />
-        <Stack.Screen name="Current" component={CurrentBrews}
-          options={{
-            headerTitle: "Brews in Progress"
-          }}
-        />
-        <Stack.Screen name="Add" component={AddBrew}
+        <Stack.Screen
+          name="Add"
+          component={AddBrew}
           options={{
             headerTitle: "Add Brew"
           }}
