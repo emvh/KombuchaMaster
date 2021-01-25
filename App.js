592236d186730e98aba69e61856d44336f06efb3
emvh <emilyvho@gmail.com>
2021-01-25 02:22:56 -0800

    Add native base list avatar to current brews

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ const Stack = createStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Add">
+      <Stack.Navigator initialRouteName="Navigation">
         <Stack.Screen
           name="Navigation"
           component={NavigationTabs}
--- src/screens/CurrentBrews.js
+++ src/screens/CurrentBrews.js
@@ -1,36 +1,41 @@
 import React, { useEffect } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { Container, Header, Content, List, ListItem, Left, Body, Right, Thumbnail, Text } from 'native-base';
 
 const CurrentBrews = () => {
 
-  // const navigation = useNavigation();
-
-  // useEffect(() => {
-  //   navigation.setOptions({
-  //     headerRight: () => (
-  //       <Button
-  //         onPress={() => navigation.navigate('Add')}
-  //         title="Add"
-  //         color="#4287f5"
-  //       />
-  //     )
-  //   })
-  // })
-
   return (
-    <View style={styles.container}>
-      <Text>hello current brew babies</Text>
-    </View>
-  )
+    // <View style={styles.container}>
+    //   <Text>hello current brew babies</Text>
+    // </View>
+      <Container>
+          <Content>
+            <List>
+              <ListItem avatar>
+                <Left>
+                  <Thumbnail source={{ uri: 'Image URL' }} />
+                </Left>
+                <Body>
+                  <Text>brew name</Text>
+                  <Text note>end date ... </Text>
+                </Body>
+                {/* <Right>
+                  <Text note>3:43 pm</Text>
+                </Right> */}
+              </ListItem>
+            </List>
+          </Content>
+      </Container>
+  );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
 
 export default CurrentBrews;
