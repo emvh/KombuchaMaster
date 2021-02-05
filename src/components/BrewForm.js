3d2472a50a4d4aeff0cbde24026b65de61e031ba
emvh <emilyvho@gmail.com>
2021-02-05 04:23:48 -0800

    Update styling

--- src/components/BrewForm.js
+++ src/components/BrewForm.js
@@ -1,7 +1,7 @@
 import React, { Component, useState, useCallback, useEffect } from 'react';
 import { Button, StyleSheet, TextInput, TouchableOpacity, View } from 'react-native';
 
-import { Container, Header, Content, Form, Icon, Item, Input, Label, Picker, Text, Textarea } from 'native-base';
+import { Container, Header, Content, Form, Icon, Item, Input, Label, ListItem, Picker, Separator, Text, Textarea } from 'native-base';
 import DateTimePickerModal from "react-native-modal-datetime-picker";
 import { getBrewDays } from '../utils/GetBrewDays.js'
 
@@ -10,7 +10,7 @@ const BrewForm = () => {
   const [startDate, setStartDate] = useState();
   const [formDate, setFormDate] = useState();
   const [endDate, setEndDate] = useState();
-  const [selected2, setSelect] = useState();
+  const [brewDays, setBrewDays] = useState();
   const [tea, setTeaType] = useState();
 
   const showDatePicker = () => {
@@ -18,7 +18,6 @@ const BrewForm = () => {
   };
 
   const hideDatePicker = () => {
-    // console.warn("A date has been picked: ", date);
     setDatePickerVisibility(false);
   };
 
@@ -29,8 +28,8 @@ const BrewForm = () => {
     hideDatePicker();
   };
 
-  const onValueChange2 = (value: string) => {
-    setSelect(value);
+  const onBrewDaysValueChange = (value: string) => {
+    setBrewDays(value);
   };
 
   const onTeaValueChange = (value: string) => {
@@ -45,43 +44,44 @@ const BrewForm = () => {
   }
 
   useEffect(() => {
-    if (formDate && selected2) {
-      const days = getBrewDays(selected2);
+    if (formDate && brewDays) {
+      const days = getBrewDays(brewDays);
       const end = addDays(startDate, days);
       setEndDate(end);
     }
   })
 
   return (
-      <Container>
+      <Container style={styles.container}>
+
         <Form>
+          <ListItem itemDivider/>
 
           <Item fixedLabel>
             <Label>Brew Name</Label>
             <Input />
           </Item>
 
-          <Text>1st Fermentation</Text>
+          <ListItem itemDivider/>
 
-          <Item fixedLabel onPress={showDatePicker}>
+          <ListItem itemDivider>
+            <Text>1st Fermentation</Text>
+          </ListItem>
+
+          <Item fixedLabel onPress={showDatePicker} >
             <Label>Start Date</Label>
-            <Input>{formDate}</Input>
-            {/* <Text>{formDate}</Text> */}
+            <Text style={styles.date}>{formDate}</Text>
           </Item>
 
           <Item fixedLabel>
           <Label>Brewing Days</Label>
-
-            <Item picker>
+            <Item picker style={styles.brewDaysPicker}>
                 <Picker
                   mode="dropdown"
-                  iosIcon={<Icon name="arrow-down" />}
-                  style={{ width: undefined }}
-                  placeholder="Select # days"
+                  placeholder="# Days"
                   placeholderStyle={{ color: "#bfc6ea" }}
-                  placeholderIconColor="#007aff"
-                  selectedValue={selected2}
-                  onValueChange={onValueChange2.bind(null)}
+                  selectedValue={brewDays}
+                  onValueChange={onBrewDaysValueChange.bind(null)}
                 >
                   <Picker.Item label="1" value="key0" />
                   <Picker.Item label="2" value="key1" />
@@ -92,62 +92,72 @@ const BrewForm = () => {
               </Item>
           </Item>
 
-          <Item fixedLabel >
+          <Item fixedLabel>
             <Label>End Date</Label>
-            <Input>{endDate}</Input>
-            {/* <Text>{endDate}</Text> */}
+            <Text style={styles.date}>{endDate}</Text>
           </Item>
 
-          <Text>Ingredients</Text>
-          <Text>2nd Fermentation</Text>
+          <ListItem itemDivider/>
+          <ListItem itemDivider>
+            <Text>Ingredients</Text>
+          </ListItem>
 
-          <Item fixedLabel>
-            <Label>Tea</Label>
-            <Input/><Text>Tbsp/Teabags                </Text>
-            <Item picker>
-              <Picker
-                mode="dropdown"
-                // iosIcon={<Icon name="arrow-down" />}
-                style={{ width: undefined }}
-                placeholder="Tea Type"
-                placeholderStyle={{ color: "#bfc6ea" }}
-                // placeholderIconColor="#007aff"
-                selectedValue={tea}
-                onValueChange={onTeaValueChange.bind(null)}
-              >
-                <Picker.Item label="Black" value="key0" />
-                <Picker.Item label="Green" value="key1" />
-                <Picker.Item label="Oolong" value="key2" />
-                <Picker.Item label="White" value="key3" />
-                <Picker.Item label="Other" value="key4" />
-              </Picker>
+            <Item fixedLabel>
+              <Label>Tea</Label>
+              <Input style={styles.teaInput} placeholderTextColor="#bfc6ea" placeholder='#'></Input>
+              <Text style={styles.teaMeasurement}>Tbsp/Teabags</Text>
+              <Item picker style={styles.teaType}>
+                <Picker
+                  mode="dropdown"
+                  style={{ width: undefined }}
+                  placeholder="Tea Type"
+                  placeholderStyle={{ color: "#bfc6ea" }}
+                  placeholderIconColor="#007aff"
+                  selectedValue={tea}
+                  onValueChange={onTeaValueChange.bind(null)}
+                >
+                  <Picker.Item label="Black" value="key0" />
+                  <Picker.Item label="Green" value="key1" />
+                  <Picker.Item label="Oolong" value="key2" />
+                  <Picker.Item label="White" value="key3" />
+                  <Picker.Item label="Other" value="key4" />
+                </Picker>
+              </Item>
             </Item>
+
+          <Item fixedLabel style={styles.itemField}>
+            <Label style={styles.ingredientLabel}>Water</Label>
+            <Input style={styles.inputCups} placeholderTextColor="#bfc6ea" placeholder='#'/>
+            <Text style={styles.cupsText}>Cups</Text>
+          </Item>
+          <Item fixedLabel>
+            <Label style={styles.ingredientLabel}>Sugar</Label>
+            <Input style={styles.inputCups} placeholderTextColor="#bfc6ea" placeholder='#'/>
+            <Text style={styles.cupsText}>Cups</Text>
+          </Item>
+          <Item fixedLabel style={styles.itemField}>
+            <Label style={styles.ingredientLabel}>Starter Tea</Label>
+            <Input style={styles.inputCups} placeholderTextColor="#bfc6ea" placeholder='#'/>
+            <Text style={styles.cupsText}>Cups</Text>
           </Item>
 
-          <Item fixedLabel>
-            <Label>Water</Label>
-            <Input /><Text>Cups</Text>
-          </Item>
-          <Item fixedLabel>
-            <Label>Sugar</Label>
-            <Input /><Text>Cups</Text>
-          </Item>
-          <Item fixedLabel>
-            <Label>Starter Tea</Label>
-            <Input /><Text>Cups</Text>
-          </Item>
+          <ListItem itemDivider/>
+          <ListItem itemDivider>
+            <Text>Notes</Text>
+          </ListItem>
 
-          <Item fixedLabel>
-            <Label>Notes</Label>
-            <Input multiline={true}
-            // numberOfLines={1}
-            style={{height: 100}}
-            />
-          </Item>
+          <Textarea rowSpan={3} placeholder="Add brew notes" />
+
+          <ListItem itemDivider/>
 
           <TouchableOpacity style={styles.button}>
             <Text>Let's Brew!</Text>
           </TouchableOpacity>
+
+          <ListItem itemDivider/>
+          <ListItem itemDivider/>
+          <ListItem itemDivider/>
+          <ListItem itemDivider/>
 
         </Form>
 
@@ -159,18 +169,55 @@ const BrewForm = () => {
         />
 
       </Container>
-
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    margin: 10,
+  },
+  date: {
+    flex: 2,
+    fontSize: 17,
+    height: 50,
+    paddingTop: 15,
+  },
+  brewDaysPicker: {
+    flex: 2.3,
+    fontSize: 17,
+    borderBottomWidth: 0.1,
+  },
+  teaInput: {
+    marginLeft: 90,
+    flex: 1,
+  },
+  teaMeasurement: {
+    flex: 3.3,
+    fontSize: 17
+  },
+  teaType: {
+    borderBottomWidth: 0,
+  },
+  ingredientLabel: {
+    flex: 2,
+    width: 100,
+  },
+  inputCups: {
+    marginLeft: 30,
+    flex: 0.8,
+  },
+  cupsText: {
+    fontSize: 17,
+    flex: 1.9,
+    paddingRight: 120
+  },
   button: {
     alignSelf: "stretch",
     alignItems: "center",
     padding: 20,
     backgroundColor: "#59cbbd",
-    marginTop: 30
-  }
+  },
 });
 
 export default BrewForm;
