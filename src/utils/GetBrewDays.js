e95e05d570bea5251aee2fbb55450a3b65197357
emvh <emilyvho@gmail.com>
2021-02-05 05:00:20 -0800

    Simplify code

--- src/utils/GetBrewDays.js
+++ src/utils/GetBrewDays.js
@@ -1,14 +1,6 @@
 import React from 'react';
 
 export function getBrewDays(value) {
-  switch(value) {
-    case 'key0':
-      return 1;
-    case 'key1':
-      return 2;
-    case 'key2':
-      return 3;
-    case 'key3':
-      return 4;
-  }
+  let days = Number(value.substr(3)) + 1;
+  return days;
 }
