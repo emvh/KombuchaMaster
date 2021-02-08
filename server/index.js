05ef876f226219bed94fbe1b73a0e5bc972dab8a
emvh <emilyvho@gmail.com>
2021-02-08 02:19:10 -0800

    Setup server

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/api/brews', (req, res) => {
+  res.send('Hello!')
+})
+
+app.listen(port, () => {
+  console.log(`KombuchaMaster listening at http://localhost:${port}`)
+})
