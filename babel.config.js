d6b24345865167b2cfbeec291529ed4cd1c6fee2
emvh <emilyvho@gmail.com>
2021-02-12 03:16:42 -0800

    Update config

--- babel.config.js
+++ babel.config.js
@@ -1,31 +1,15 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    // presets: ['babel-preset-expo'],
-    presets:['module:metro-react-native-babel-preset'],
-    plugins: [
-      ["module:react-native-dotenv", {
-        "moduleName": "@env",
-        "path": ".env",
-        "blacklist": null,
-        "whitelist": null,
-        "safe": true,
-        "allowUndefined": true
-      }]
-    ]
+    presets: [
+      "babel-preset-expo",
+      "module:metro-react-native-babel-preset"
+    ],
+    env: {
+      development: {
+        plugins: ["module:react-native-dotenv"]
+      }
+    }
   };
 };
 
-// module.exports = {
-//   presets:['module:metro-react-native-babel-preset',
-  // "plugins": [
-  //   ["module:react-native-dotenv", {
-  //     "moduleName": "@env",
-  //     "path": ".env",
-  //     "blacklist": null,
-  //     "whitelist": null,
-  //     "safe": true,
-  //     "allowUndefined": true
-  //   }]
-  // ]
-// };
