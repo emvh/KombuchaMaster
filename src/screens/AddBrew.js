a03cac566ac9b4022a4e9e53711af6d542d9f1e3
emvh <emilyvho@gmail.com>
2021-03-05 01:55:57 -0800

    Fix axios post request content type

--- src/screens/AddBrew.js
+++ src/screens/AddBrew.js
@@ -1,22 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import { StyleSheet, Text, View } from 'react-native';
 import BrewForm from '../components/BrewForm.js';
 
-const AddBrew = () => {
-  return (
-    <View style={styles.container}>
-      {/* <Text>add brew here</Text> */}
-      <BrewForm />
-    </View>
-  )
+class AddBrew extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      brews: [],
+    };
+    this.addToBrewList = this.addToBrewList.bind(this);
+  }
+
+  addToBrewList(brew) {
+    console.log('addbrewpage', brew)
+    axios({
+      url: 'http://127.0.0.1:3000/api/brews',
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      data: brew,
+    })
+      .then(() => console.log('added brew'))
+      .catch(error => console.log(error))
+  }
+
+  render() {
+    const { navigation } = this.props;
+    return (
+      <View style={styles.container}>
+        <BrewForm
+          navigation={navigation}
+          addToBrewList={this.addToBrewList}
+        />
+      </View>
+    );
+  };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
   },
 });
 
