30e60007f317f371353e5bbbb7b90d24c97f4628
emvh <emilyvho@gmail.com>
2021-03-11 00:17:20 -0800

    Wrap App with AppContext provider

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import * as React from 'react';
+import React, { useContext } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
@@ -9,38 +9,41 @@ import AddBrew from './src/screens/AddBrew.js';
 import CompletedBrews from './src/screens/CompletedBrews.js';
 import Login from './src/screens/Login.js';
 import Signup from './src/screens/Signup.js';
-import Firebase from './src/firebase/config.js';
+import { AppContext, AppContextProvider } from './src/contexts/AppContext.js'
 
 const Stack = createStackNavigator();
 
 const App = () => {
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Navigation">
-        <Stack.Screen
-          name="Login"
-          component={Login}
-        />
-        <Stack.Screen
-          name="Signup"
-          component={Signup}
-        />
-        <Stack.Screen
-          name="Navigation"
-          component={NavigationTabs}
-        />
-        <Stack.Screen
-          name="Add"
-          component={AddBrew}
-          options={{
-            headerTitle: "Add Brew",
-            headerBackTitle: "Back"
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <AppContextProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Navigation">
+          <Stack.Screen
+            name="Login"
+            component={Login}
+          />
+          <Stack.Screen
+            name="Signup"
+            component={Signup}
+          />
+          <Stack.Screen
+            name="Navigation"
+            component={NavigationTabs}
+          />
+          <Stack.Screen
+            name="Add"
+            component={AddBrew}
+            options={{
+              headerTitle: "Add Brew",
+              headerBackTitle: "Back"
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+   </AppContextProvider>
   );
+
 }
 
 export default App;
